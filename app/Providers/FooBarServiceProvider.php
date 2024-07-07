52dfef445a6d873ab36c3dce8c7a7f0098418243
Cell6969 <bossmarinoo@gmail.com>
2024-07-08 00:18:00 +0700

    Update: Service Provider Case

---
+++ app/Providers/FooBarServiceProvider.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Providers;
+
+use App\Data\Bar;
+use App\Data\Foo;
+use App\Services\HelloService;
+use App\Services\HelloServiceIndonesia;
+use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\Support\ServiceProvider;
+
+class FooBarServiceProvider extends ServiceProvider implements DeferrableProvider
+{
+    public array $singletons = [
+        HelloService::class => HelloServiceIndonesia::class
+    ];
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        echo "Foo bar service provider";
+        $this->app->singleton(Foo::class, function ($app) {
+            return new Foo();
+        });
+
+        $this->app->singleton(Bar::class, function ($app) {
+            // $foo = 
+            return new Bar($app->make(Foo::class));
+        });
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Override provides
+     * @return void
+     */
+
+    public function provides()
+    {
+        return [
+            HelloService::class,
+            Foo::class,
+            Bar::class
+        ];
+    }
+}
